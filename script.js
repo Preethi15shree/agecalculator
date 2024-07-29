55b88b3f96bcff1dd8f1cb24fc74912bc67f834d
Preethi15shree <preethishreeb2004@gmail.com>
2024-07-29 09:36:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function ageCalculate() {
+    const dateInput = document.getElementById('date-input').value;
+    const birthDate = new Date(dateInput);
+    const today = new Date();
+    
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+        months--;
+        days += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    document.getElementById('years').textContent = years;
+    document.getElementById('months').textContent = months;
+    document.getElementById('days').textContent = days;
+
+    // Add the show class to trigger the animation
+    document.querySelector('.outputs-wrapper').classList.add('show');
+}
